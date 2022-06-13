877cafe07edab0eccdbeb3b8c92b783b2e31f5f1
RaneNjims16 <njims2011@gmail.com>
2022-06-13 21:23:36 +0100

    styled my calculator

---
+++ Calculator/App1/JS/main.js
@@ -0,0 +1,9 @@
+const buttonNumbers = document.querySelectorAll("data-number");
+const buttonSign = document.querySelectorAll("data-operators");
+const buttonDelete = document.querySelector("data-delete");
+const buttonClean = document.querySelector("data-clean");
+const buttonDot = document.querySelector("data-dot");
+const buttonEqual= document.querySelector("data-equal");
+const outputResult = document.querySelector("data-result");
+const outputCalculation = document.querySelector("data-Calculation");
+/ * ****************************** */
